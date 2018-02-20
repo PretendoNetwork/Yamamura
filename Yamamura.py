21aae1a3da96d25e3edb784e5fa5ae6c657b01af
Superwhiskers <kspy360@rocketmail.com>
2018-02-20 16:46:32 -0600

    more things

--- Yamamura.py
+++ Yamamura.py
@@ -8,13 +8,19 @@ client = Bot(description="Yamamura by superwhiskers", command_prefix=".", pm_hel
 
 @client.event
 async def on_ready():
+
+    # print some output
     print('logged in as: '+client.user.name+' (id:'+client.user.id+') | connected to '+str(len(client.servers))+' server(s)')
     print('invite: https://discordapp.com/oauth2/authorize?client_id={}&scope=bot&permissions=8'.format(client.user.id))
     return await client.change_presence(game=discord.Game(name='with edamame'))
 
-@client.command()
-async def ping():
-    """ping pong"""
-    await client.say("pong")
-    
+@client.event
+async def on_message(msg):
+    if (msg.channel.name == "voting") or (msg.channel.name == "voting-game-suggestions"):
+        # print(msg.server.emojis[0].name)
+        await client.add_reaction(msg, u'\U0001F44D')
+        await client.add_reaction(msg, u'\U0001F44E')
+    if msg.content == "ayy":
+        await client.send_message(msg.channel, "lmao")
+
 client.run('place key here')
