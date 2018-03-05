f050c126c8bc40e1a6960685add6abf4d3b229a1
superwhiskers <kspy360@rocketmail.com>
2018-03-05 14:00:15 -0600

    add a better logreader that doesn't update each second

--- logreader.sh
+++ logreader.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+oldsum="$(md5sum output.log)"
+clear
+echo "Yamamura chat log:"
+echo "----------------------------"
+tail -n 20 output.log
 while [[ True ]]; do
-	clear
-	echo "Pretendo Discord log:"
-	echo "---------------------------"
-	tail -n 20 output.log
-	sleep 5
+	newsum="$(md5sum output.log)"
+	if [[ "$oldsum" != "$newsum" ]]; then
+		clear
+		echo "Yamamura chat log:"
+		echo "----------------------------"
+		tail -n 20 output.log
+		oldsum="$newsum"
+	fi
+	sleep 2
 done
