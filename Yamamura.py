974bfc872445dccff3a7e416aadaeca4b35cc069
Iamnihc <34426008+Iamnihc@users.noreply.github.com>
2018-03-06 15:03:33 -0800

    locked bot to certain channels

--- Yamamura.py
+++ Yamamura.py
@@ -58,6 +58,7 @@ superwhiskers (@!superwhiskers™#3210): bot concept and main developer
 Netux         (@Netux#2308): certain features and some regex work
 Pika          (@《ThatNerdyPikachu》#2849): \"features\" and working on rewrite```"""
 
+
 # currently composing people
 composing = []
 try:
@@ -367,6 +368,12 @@ try:
         @bot.event
         async def on_message(msg):
 
+            #Force the things to work on SPAM CHANNELS only. ONLY
+            if not msg.channel.id in cfg["spam-channels"]:
+                #use gets used later in the code
+                use = False
+            else:
+                use = True
             # log-em.
             log(f"[{ msg.author.name } in { msg.channel.name }]: { msg.content } [{ strftime('%m/%d/%Y %H:%M:%S', gmtime()) }]")
 
@@ -403,6 +410,9 @@ try:
                 await bot.delete_message(msg)
 
             # i'd just like to interject for a moment...
+
+
+
             elif "linux" in msg.content.lower():
 
                 # make sure it is only linux
@@ -411,7 +421,6 @@ try:
 
                 # search for linux
                 interject = False
-                punct=False
 
                 for x in range(0, len(splitmsg)):
                     if splitmsg[x].lower() == "linux":
@@ -420,15 +429,15 @@ try:
                     elif len(splitmsg[x])>5 and splitmsg[x].lower()[0,5] == "linux" and not splitmsg[x][6].isalpha:
                         interject=True
                         break
-                if str(msg.channel)=="414517142390702091" or str(msg.channel)=="408723233030799362":
-                    interject=False
 
                 # interject
-                if interject == True:
+                if interject == True and use:
                     await bot.send_message(msg.channel, f"""I'd just like to interject for a moment, { msg.author.mention }. What you're referring to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX. Many computer users run a modified version of the GNU system every day, without realizing it. Through a peculiar turn of events, the version of GNU which is widely used today is often called "Linux", and many of its users are not aware that it is basically the GNU system, developed by the GNU Project. There really is a Linux, and these people are using it, but it is just a part of the system they use. Linux is the kernel: the program in the system that allocates the machine's resources to the other programs that you run. The kernel is an essential part of an operating system, but useless by itself; it can only function in the context of a complete operating system. Linux is normally used in combination with the GNU operating system: the whole system is basically GNU with Linux added, or GNU/Linux. All the so-called "Linux" distributions are really distributions of GNU/Linux.""")
                     return
 
-            elif "reduxredstone" in msg.content.lower():
+            elif "reduxredstone" in msg.content.lower() and use:
+
+
                 splitmsg = msg.content.split(" ")
                 sendgudmeme = False
                 for x in range(0, len(splitmsg)):
@@ -440,7 +449,7 @@ try:
                     return
 
             # eh ayy?
-            elif "ay" in msg.content.lower():
+            elif "ay" in msg.content.lower() and use:
 
                 # no everyones
                 if ("@everyone" in msg.content) or ("@here" in msg.content):
