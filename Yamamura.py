7a4013370a397dad30a949ec8b6361fcb5eb3ff0
Pika <15848969+ThatNerdyPikachu@users.noreply.github.com>
2018-03-07 18:24:50 -0500

    Fixed whole bot

--- Yamamura.py
+++ Yamamura.py
@@ -49,7 +49,6 @@ Mail:
     {cfg["prefix"]}mail all                             : Read all mail (mods only)
     {cfg["prefix"]}mail clean                           : Clean mail read by all mods (mods only)
     {cfg["prefix"]}mail delete <id>                     : Delete mail by id (mods only)
-    {cfg["prefix"]}status                               : Check the status of the offical Pretendo servers
 ```"""
 
 # author message
@@ -398,8 +397,8 @@ try:
             # voting made easy
             if type(msg.channel) != discord.DMChannel:
                 if (msg.channel.name == "voting") or (msg.channel.name == "yamamura-suggestions") or (msg.channel.name == "voting-game-suggestions"):
-                    await msg.add_reaction(u'\U0001F44D')
-                    await msg.add_reaction(u'\U0001F44E')
+                        await msg.add_reaction(u'\U0001F44D')
+                        await msg.add_reaction(u'\U0001F44E')
 
             # do you like teapots? dun dun dun dun dunnn....
             elif "i'm a teapot" in msg.content.lower():
@@ -430,7 +429,7 @@ try:
 
                 # interject
                 if interject == True:
-                    await bot.send_message(msg.channel, f"""I'd just like to interject for a moment, { msg.author.mention }. What you're referring to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX. Many computer users run a modified version of the GNU system every day, without realizing it. Through a peculiar turn of events, the version of GNU which is widely used today is often called "Linux", and many of its users are not aware that it is basically the GNU system, developed by the GNU Project. There really is a Linux, and these people are using it, but it is just a part of the system they use. Linux is the kernel: the program in the system that allocates the machine's resources to the other programs that you run. The kernel is an essential part of an operating system, but useless by itself; it can only function in the context of a complete operating system. Linux is normally used in combination with the GNU operating system: the whole system is basically GNU with Linux added, or GNU/Linux. All the so-called "Linux" distributions are really distributions of GNU/Linux.""")
+                    await msg.channel.send(f"""I'd just like to interject for a moment, { msg.author.mention }. What you're referring to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX. Many computer users run a modified version of the GNU system every day, without realizing it. Through a peculiar turn of events, the version of GNU which is widely used today is often called "Linux", and many of its users are not aware that it is basically the GNU system, developed by the GNU Project. There really is a Linux, and these people are using it, but it is just a part of the system they use. Linux is the kernel: the program in the system that allocates the machine's resources to the other programs that you run. The kernel is an essential part of an operating system, but useless by itself; it can only function in the context of a complete operating system. Linux is normally used in combination with the GNU operating system: the whole system is basically GNU with Linux added, or GNU/Linux. All the so-called "Linux" distributions are really distributions of GNU/Linux.""")
                     return
 
 	    # the redux meme
