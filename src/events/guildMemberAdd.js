5e25588511eedc0c2c206f192c6097e96a004349
Jonathan Barrow <jonbarrow1998@gmail.com>
2022-05-06 18:23:00 -0400

    Moved events to files

---
+++ src/events/guildMemberAdd.js
@@ -0,0 +1,66 @@
+const Discord = require('discord.js');
+
+/**
+ * 
+ * @param {Discord.GuildMember} member
+ */
+function guildMemberAddHandler(member) {
+	const embed = new Discord.MessageEmbed();
+
+	embed.setColor(0x1B1F3B);
+	embed.setTitle('Pretendo Network');
+	embed.setURL('https://pretendo.network');
+	embed.setDescription('\u200b');
+	embed.setThumbnail('https://i.imgur.com/8clyKqx.png');
+	embed.setImage('https://i.imgur.com/CF7qgW1.png');
+	embed.addFields([
+		{
+			name: '📃 Social Media',
+			value: '\u200b'
+		},
+		{
+			name: '<:patreonlogo:886254233786138635> Patreon',
+			value: 'https://patreon.com/PretendoNetwork'
+		},
+		{
+			name: '<:twitterlogo:886254233962291241> Twitter',
+			value: 'https://twitter.com/PretendoNetwork'
+		},
+		{
+			name: '<:twitchlogo:886254234201362473> Twitch',
+			value: 'https://twitch.tv/PretendoNetwork'
+		},
+		{
+			name: '<:youtubelogo:886254234226528337> YouTube',
+			value: 'https://youtube.com/c/PretendoNetwork'
+		},
+		{
+			name: '\u200b',
+			value: '\u200b'
+		},
+		{
+			name: '<:rulestext:886254514141806643> Rules',
+			value: '\u200b'
+		},
+		{
+			name: ':one:',
+			value: 'No advertising unless explicitly allowed to do so by one of the developers'
+		},
+		{
+			name: ':two:',
+			value: 'Do not share anything illegal. This includes illegal game/fw dumps, any console SDK, etc. We are unsure as to what is illegal to share, so keep all that to the DMs just to be safe'
+		},
+		{
+			name: ':three:',
+			value: 'Respect channel names and topics. Offtopic chat goes in the offtopic channel'
+		},
+		{
+			name: ':four:',
+			value: 'Be kind. If someone asks a question you can help with, be nice about helping them. Know your audience'
+		}
+	]);
+
+	member.send('Thank you for joining the Pretendo Network Discord server! Check below for some server information and links', { embed });
+}
+
+module.exports = guildMemberAddHandler;
