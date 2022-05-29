63d7fb236c17bf18e06e556348b23b91722c7e39
Jonathan Barrow <jonbarrow1998@gmail.com>
2022-05-28 22:32:30 -0400

    Removed unused message handler

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -12,19 +12,6 @@ async function messageCreateHandler(message) {
 		message.reply('Looks like you tried to use a legacy command! Try our new slash commands by just typing "/"!');
 		return;
 	}
-
-	const channel = await message.channel.fetch();
-	const guild = await channel.guild.fetch();
-	const roles = await guild.roles.fetch();
-	const memberRole = roles.find(role => role.name === 'member');
-	const unverifiedRole = roles.find(role => role.name === 'unverified');
-
-	if (channel.name === 'readme' && channel.type === 'GUILD_TEXT' && channel.parent?.name === 'pretendo') {
-		const member = await message.member.fetch();
-		if (!member.roles.cache.has(memberRole.id) && member.roles.cache.has(unverifiedRole.id)) {
-			await member.roles.add(unverifiedRole);
-		}
-	}
 }
 
 module.exports = messageCreateHandler;
