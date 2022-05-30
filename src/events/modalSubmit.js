6dfd363e23935204d615e2fe9bbfc3e4d394cc5f
Jelle van Snik <jellevs@gmail.com>
2022-05-30 17:29:19 +0200

    warp modal submit in try catch

--- src/events/modalSubmit.js
+++ src/events/modalSubmit.js
@@ -6,23 +6,23 @@ const { ModalSubmitInteraction } = require('discord-modals');
  * @param {ModalSubmitInteraction} interaction
  */
 async function modalSubmitHandler(interaction) {
-	const { customId } = interaction;
-
-	/** @type {Discord.Collection} */
-	const modals = interaction.client.modals;
-	const modal = modals.find(modal => customId.startsWith(modal.name)); // hack to be able to append extra metadata to modals
-
-	// do nothing if no modal
-	if (!modal) {
-		interaction.reply(`Missing modal handler for \`${customId}\``);
-		return;
-	}
-
-	// run the modal
 	try {
+		const { customId } = interaction;
+
+		/** @type {Discord.Collection} */
+		const modals = interaction.client.modals;
+		const modal = modals.find(modal => customId.startsWith(modal.name)); // hack to be able to append extra metadata to modals
+
+		// do nothing if no modal
+		if (!modal) {
+			interaction.reply(`Missing modal handler for \`${customId}\``);
+			return;
+		}
+
+		// run the modal
 		await modal.handler(interaction);
-	} catch {
-		console.error('handler failed modal');
+	} catch (err) {
+		console.error('modal submit failed to be handled', err);
 	}
 }
 
